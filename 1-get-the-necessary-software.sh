b6f251b21cac7ae93f74e27f09c2de091455952e
Erik Dubois <erik.dubois@gmail.com>
2021-11-13 10:28:00 +0100

    Comment : update on Sat Nov 13 10:28:00 AM CET 2021

--- 1-get-the-necessary-software.sh
+++ 1-get-the-necessary-software.sh
@@ -20,8 +20,6 @@
 #sudo pacman -S git gnome-themes-standard gtk-engine-murrine autoconf automake pkg-config inkscape optipng gnome-shell sassc meson --needed
 sudo pacman -S inkscape sassc meson gnome-themes-standard gtk-engine-murrine gnome-shell pkg-config cinnamon gtk3 optipng --needed --noconfirm
 
-echo "Get the code for Openbox and put it in /tmp"
-
 echo "################################################################"
 echo "Next up figure out the colour accent of your new Arc theme online or with gpick or other software."
 echo "Then change the colour for the variable NEWCOLOUR1 in next script, save and run it."
